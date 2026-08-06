ae00c63c6993b23f683ab5d27d043aac558fbe2f
Kaung-HtetKyaw <kaunghtetkyaw02749@gmail.com>
2026-08-06 23:02:29 +0700

    feat: init save commander

---
+++ src/commander/save.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+const resp = @import("../resp.zig");
+const store = @import("../store.zig");
+const Commander = @import("interface.zig");
+const TestHelpers = @import("../tests/helpers.zig");
+const DefaultStorage = @import("../storage/default_storage.zig");
+const persistence = @import("../persistence.zig");
+
+const Save = @This();
+
+allocator: std.mem.Allocator,
+arguments: []resp.RESPValue,
+
+pub fn commander(self: *Save) Commander {
+    return .{ .ptr = self, .vtable = &vtable };
+}
+
+const vtable = Commander.VTable{ .execute = execute, .deinit = deinit };
+
+fn execute(_: *anyopaque, _: std.Io, _: *store.Store, _: *Commander.ClientState) Commander.Error!resp.RESPValue {
+    return .{ .simple_string = "OK" };
+}
+
+fn deinit(ptr: *anyopaque) void {
+    const self: *Save = @ptrCast(@alignCast(ptr));
+    self.allocator.destroy(self);
+}
