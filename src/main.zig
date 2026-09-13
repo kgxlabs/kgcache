7090e8cb0e15ab842cd35a9ea53bca5a733c938a
Kaung-HtetKyaw <kaunghtetkyaw02749@gmail.com>
2026-09-13 20:41:28 +0700

    feat: wire logger in server

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,18 @@
 const std = @import("std");
 const Config = @import("config.zig");
 const Server = @import("server.zig");
+const logging = @import("logger.zig");
 
 pub fn main(init: std.process.Init) !void {
+    var default_logger = logging.DefaultLogger.init(init.io);
+    const logger = default_logger.logger();
+
     const config = try Config.loadFromArgs(init);
 
     var gpa: std.heap.DebugAllocator(.{}) = .init;
     defer _ = gpa.deinit();
 
-    const server = try Server.create(init.io, gpa.allocator(), config);
+    const server = try Server.create(init.io, gpa.allocator(), config, logger);
     defer server.destroy();
 
     // TODO: install SIGINT/SIGTERM shutdown handling after acceptLoop can be
